5dc3f8fe32aa4dd08c946a505d6d25cb154c947e
Roch Goszczyński <roch.goszczynski@gmail.com>
2020-06-09 21:51:13 +0200

    Add case models and change buttons configuration

--- STM32/Debug/Core/Src/main.d
+++ STM32/Debug/Core/Src/main.d
@@ -36,7 +36,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
  ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
  ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h \
- ../Core/Inc/5IO_Keypad.h
+ ../Core/Inc/5IO_Keypad.h ../Core/Inc/Types.h
 
 ../Core/Inc/main.h:
 
@@ -115,3 +115,5 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h:
 
 ../Core/Inc/5IO_Keypad.h:
+
+../Core/Inc/Types.h:
